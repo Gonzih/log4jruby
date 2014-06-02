0965ddb6331ba84fe4eb124b27eebcf8a98dfec3
Max Gonzih <gonzih@gmail.com>
2014-06-02 12:01:37 +0200

    Update java require statements

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,7 +8,8 @@
 
 require 'java'
 
-require File.dirname(__FILE__) + '/../log4j/log4j-1.2.16.jar'
+require File.dirname(__FILE__) + '/../log4j/log4j-core-2.0-rc1.jar'
+require File.dirname(__FILE__) + '/../log4j/log4j-api-2.0-rc1.jar'
 
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
