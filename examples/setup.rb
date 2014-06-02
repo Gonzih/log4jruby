bd977741d0c7d1fb89beaca17869ecfe14a43e71
Max Gonzih <gonzih@gmail.com>
2014-06-02 11:58:13 +0200

    Update log4j jar

--- examples/setup.rb
+++ examples/setup.rb
@@ -4,5 +4,5 @@ $CLASSPATH << File.dirname(__FILE__) + "/"
 
 $LOAD_PATH << File.dirname(__FILE__) + '/../lib'
 
-
-require File.dirname(__FILE__) + '/../log4j/log4j-1.2.16.jar'
+require File.dirname(__FILE__) + '/../log4j/log4j-core-2.0-rc1.jar'
+require File.dirname(__FILE__) + '/../log4j/log4j-api-2.0-rc1.jar'
